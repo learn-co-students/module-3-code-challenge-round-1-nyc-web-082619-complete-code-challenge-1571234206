65e63ff9d1528a5e21e57842afdd4e55ab430959
James Sharpe <jameswrightsharpe@gmail.com>
2019-10-16 10:58:41 -0400

    crushed graphical bug on beer selection caused by rerendering the entire detail

--- src/index.js
+++ src/index.js
@@ -7,24 +7,7 @@ const detail = document.querySelector("#beer-detail");
 
 fetch(BEERS_PATH)
   .then(e => e.json())
-  .then(e => e.forEach(renderBeer));
-
-function renderBeer(beer) {
-  list.insertAdjacentHTML(
-    "beforeend",
-    `<li data-id="${beer.id}" class="list-group-item">${beer.name}</li>`
-  );
-}
-
-function renderBeerDetail(beer) {
-  detail.innerHTML = `
-    <h1>${beer.name}</h1>
-    <img src="${beer.image_url}"/>
-    <h3>${beer.tagline}</h3>
-    <textarea rows=8 id='beer-description'>${beer.description}</textarea>
-    <button data-id="${beer.id}" class="btn btn-info">Save</button>
-`;
-}
+  .then(e => e.forEach(renderBeerItem));
 
 list.addEventListener("click", e => {
   if (e.target.className === "list-group-item") {
@@ -54,3 +37,42 @@ detail.addEventListener("click", e => {
     fetch(BEER_PATH(id), options).catch(alert);
   }
 });
+
+function renderBeerItem(beer) {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li data-id="${beer.id}" class="list-group-item">${beer.name}</li>`
+  );
+}
+
+function renderBeerDetail(beer) {
+  if (detail.innerHTML) {
+    updateBeerDetail(beer);
+  } else {
+    detail.innerHTML = `
+    <h1>${beer.name}</h1>
+    <img src="${beer.image_url}"/>
+    <h3>${beer.tagline}</h3>
+    <textarea rows=8 id='beer-description'>${beer.description}</textarea>
+    <button data-id="${beer.id}" class="btn btn-info">Save</button>
+`;
+  }
+}
+
+//updateBeerDetail removes the graphical bug caused by re-rendering the div.
+function updateBeerDetail(beer) {
+  const h1 = document.querySelector("#beer-detail > h1");
+  h1.innerText = beer.name;
+
+  const img = document.querySelector("#beer-detail > img");
+  img.src = beer.image_url;
+
+  const h3 = document.querySelector("#beer-detail > h3");
+  h3.innerText = beer.tagline;
+
+  const textarea = document.querySelector("#beer-detail > textarea");
+  textarea.value = beer.description;
+
+  const button = document.querySelector("#beer-detail > button");
+  button.dataset.id = beer.id;
+}
